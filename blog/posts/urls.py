d3c5c354f94d6b1db09eb4f659681439a69dbf28
Ruzhevych Veniamin <ben.ruzhevych@icloud.com>
2024-12-16 20:53:05 +0200

    fix

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name="news"
+app_name="posts"
 
 urlpatterns = [
     path('', views.news_list, name="list"),
     path('post/new/', views.post_create, name='create'),
+    path('post/<int:pk>/delete/', views.post_delete, name='delete')
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,22 +1,37 @@
-from django.shortcuts import redirect, render
-from django.contrib.auth.decorators import login_required
-from . import forms
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+import os
+from django.conf import settings
+from django.core.files.storage import default_storage
  
 # Create your views here.
 def news_list(request):
     posts = Post.objects.all()
-    return render(request, "news_list.html", {'posts': posts})
+    return render(request, "posts_list.html", {'posts': posts})
 # Створення нового посту
 @login_required
 def post_create(request):
     if request.method == 'POST':
-        form = forms.PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)  
             post.author = request.user      
             post.save()   
-            return redirect('news:list')
+            return redirect('posts:list')
     else:
-        form = forms.PostForm()
+        form = PostForm()
     return render(request, 'posts/post_form.html', {'form': form})
+
+
+# Видалення посту
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if post.image:
+        old_image_path = os.path.join(settings.MEDIA_ROOT, post.image.name)
+        if os.path.exists(old_image_path):
+            os.remove(old_image_path)
+    post.delete()
+    return redirect('posts:list')
